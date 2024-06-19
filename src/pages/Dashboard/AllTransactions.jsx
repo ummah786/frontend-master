32d2ec7f54410e11f21a6b18ca95504c6aafd09c
sardarjahan92 <80624010+sardarjahan92@users.noreply.github.com>
2024-06-20 00:40:29 +0530

    20 Jun

--- src/pages/Dashboard/AllTransactions.jsx
+++ src/pages/Dashboard/AllTransactions.jsx
@@ -1,51 +1,130 @@
-import React from 'react';
-import {Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material';
-import {useNavigate} from 'react-router-dom';
-
-const transactions = [
-    {date: '28 May 2024', type: 'Purchase Record', txnNo: 1, partyName: 'sandeep', amount: '₹1,000'},
-    {date: '28 May 2024', type: 'Sales Invoice', txnNo: 3, partyName: 'Cash Sale', amount: '₹200'},
-    {date: '28 May 2024', type: 'Sales Invoice', txnNo: 2, partyName: 'Cash Sale', amount: '₹200'},
-    {date: '23 May 2024', type: 'Sales Invoice', txnNo: 1, partyName: 'Raju', amount: '₹100'},
-    // Add more transactions as needed
-];
+import React, { useEffect, useState } from 'react';
+import {
+    Button,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TablePagination
+} from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {StyledTableCell} from "../../commonStyle";
+import IconButton from "@mui/material/IconButton";
+import ArticleIcon from "@mui/icons-material/Article";
 
 const AllTransactions = () => {
     const navigate = useNavigate();
+    const { salePurchaseUser } = useSelector(
+        (state) => state.salePurchaseReducerValue
+    );
+    const [filterSalePurchase, setFilterSalePurchase] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [flagView, setFlagView] = useState(false);
+    const [idFlagView, setIdFlagView] = useState('');
+    const handleBooleanChangeView = (id) => {
+        setFlagView((prevState) => !prevState);
+        setIdFlagView(id);
+    };
+
+    useEffect(() => {
+        if (Array.isArray(salePurchaseUser)) {
+            let filteredData = salePurchaseUser;
+            setFilterSalePurchase(filteredData);
+        }
+    }, [salePurchaseUser]);
 
     const handleBackClick = () => {
         navigate('/');
     };
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const formatDate = (dateString) => {
+        if (!dateString) return '';
+        const date = new Date(dateString);
+        if (isNaN(date.getTime())) {
+            // Invalid date
+            return '';
+        }
+        const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+        return new Intl.DateTimeFormat('en-CA', options).format(date);
+    };
+
     return (
         <>
-            <Button variant="contained" color="primary" onClick={handleBackClick} style={{marginBottom: '10px'}}>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleBackClick}
+                style={{ marginBottom: '10px' }}
+            >
                 Back to Dashboard
             </Button>
             <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell sx={{fontWeight: 'bold'}}>Date</TableCell>
-                            <TableCell sx={{fontWeight: 'bold'}}>Type</TableCell>
-                            <TableCell sx={{fontWeight: 'bold'}}>Txn No</TableCell>
-                            <TableCell sx={{fontWeight: 'bold'}}>Party Name</TableCell>
-                            <TableCell sx={{fontWeight: 'bold'}}>Amount</TableCell>
+                            <StyledTableCell sx={{ fontWeight: 'bold' }}>Date</StyledTableCell>
+                            <StyledTableCell sx={{ fontWeight: 'bold' }}>Type</StyledTableCell>
+                            <StyledTableCell sx={{ fontWeight: 'bold' }}>Transaction No</StyledTableCell>
+                            <StyledTableCell sx={{ fontWeight: 'bold' }}>Party Name</StyledTableCell>
+                            <StyledTableCell sx={{ fontWeight: 'bold' }}>Amount</StyledTableCell>
+                            <StyledTableCell sx={{ fontWeight: 'bold' }}>View</StyledTableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {transactions.map((txn) => (
-                            <TableRow key={txn.txnNo}>
-                                <TableCell>{txn.date}</TableCell>
-                                <TableCell>{txn.type}</TableCell>
-                                <TableCell>{txn.txnNo}</TableCell>
-                                <TableCell>{txn.partyName}</TableCell>
-                                <TableCell>{txn.amount}</TableCell>
-                            </TableRow>
-                        ))}
+                        {filterSalePurchase
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((txn) => (
+                                <TableRow key={txn.id}>
+                                    <TableCell>{formatDate(txn.creationDateTime)}</TableCell>
+                                    <TableCell>{txn.billType}</TableCell>
+                                    <TableCell>
+                                        {txn.salesInvoiceNo} {txn.purchaseInvNo}
+                                        {txn.purchaseReturnNo} {txn.purchaseNo}
+                                        {txn.quotationNo} {txn.salesReturnNo}
+                                        {txn.deliveryNo} {txn.proformaNo}
+                                        {txn.debitNoteNo} {txn.creditNoteNo}
+                                        {txn.paymentNumberIn} {txn.paymentNumberOut}
+                                    </TableCell>
+                                    <TableCell>{txn.partyName}</TableCell>
+                                    <TableCell>{txn.totalAmount}</TableCell>
+                                    <TableCell align="center">
+                                        <IconButton
+                                            aria-label="edit"
+                                            onClick={() => handleBooleanChangeView(txn.id)}
+                                        >
+                                            <ArticleIcon/>
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
                     </TableBody>
                 </Table>
-            </TableContainer>    </>
+                <TablePagination
+                    rowsPerPageOptions={[6, 10, 25]}
+                    component="div"
+                    count={filterSalePurchase.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </TableContainer>
+        </>
     );
-}
+};
 
 export default AllTransactions;
--- src/pages/Dashboard/LatestTransactions.jsx
+++ src/pages/Dashboard/LatestTransactions.jsx
@@ -1,15 +1,50 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material';
 import {Link as RouterLink} from 'react-router-dom';
+import {useSelector} from "react-redux";
+import {formatDate} from "../../commonStyle";
+import IconButton from "@mui/material/IconButton";
+import ArticleIcon from "@mui/icons-material/Article";
 
-const transactions = [
-    {date: '28 May 2024', type: 'Purchase Record', txnNo: 1, partyName: 'sandeep', amount: '₹1,000'},
-    {date: '28 May 2024', type: 'Sales Invoice', txnNo: 3, partyName: 'Cash Sale', amount: '₹200'},
-    {date: '28 May 2024', type: 'Sales Invoice', txnNo: 2, partyName: 'Cash Sale', amount: '₹200'},
-    {date: '23 May 2024', type: 'Sales Invoice', txnNo: 1, partyName: 'Raju', amount: '₹100'},
-];
 
 function LatestTransactions() {
+    const {salePurchaseUser} = useSelector(
+        (state) => state.salePurchaseReducerValue
+    );
+    const [filterSalePurchase, setFilterSalePurchase] = useState([]);
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
+    const [flagView, setFlagView] = useState(false);
+    const [idFlagView, setIdFlagView] = useState('');
+    const handleBooleanChangeView = (id) => {
+        setFlagView((prevState) => !prevState);
+        setIdFlagView(id);
+    };
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    const formatDate = (dateString) => {
+        if (!dateString) return '';
+        const date = new Date(dateString);
+        if (isNaN(date.getTime())) {
+            // Invalid date
+            return '';
+        }
+        const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+        return new Intl.DateTimeFormat('en-CA', options).format(date);
+    };
+    useEffect(() => {
+        if (Array.isArray(salePurchaseUser)) {
+            let filteredData = salePurchaseUser;
+            setFilterSalePurchase(filteredData);
+        }
+    }, [salePurchaseUser]);
     return (
         <TableContainer component={Paper}>
             <Table>
@@ -17,21 +52,38 @@ function LatestTransactions() {
                     <TableRow>
                         <TableCell sx={{fontWeight: 'bold'}}>Date</TableCell>
                         <TableCell sx={{fontWeight: 'bold'}}>Type</TableCell>
-                        <TableCell sx={{fontWeight: 'bold'}}>Txn No</TableCell>
+                        <TableCell sx={{fontWeight: 'bold'}}>Transaction No</TableCell>
                         <TableCell sx={{fontWeight: 'bold'}}>Party Name</TableCell>
                         <TableCell sx={{fontWeight: 'bold'}}>Amount</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {transactions.map((txn) => (
-                        <TableRow key={txn.txnNo}>
-                            <TableCell>{txn.date}</TableCell>
-                            <TableCell>{txn.type}</TableCell>
-                            <TableCell>{txn.txnNo}</TableCell>
-                            <TableCell>{txn.partyName}</TableCell>
-                            <TableCell>{txn.amount}</TableCell>
-                        </TableRow>
-                    ))}
+                    {filterSalePurchase
+                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        .map((txn) => (
+                            <TableRow key={txn.id}>
+                                <TableCell>{formatDate(txn.creationDateTime)}</TableCell>
+                                <TableCell>{txn.billType}</TableCell>
+                                <TableCell>
+                                    {txn.salesInvoiceNo} {txn.purchaseInvNo}
+                                    {txn.purchaseReturnNo} {txn.purchaseNo}
+                                    {txn.quotationNo} {txn.salesReturnNo}
+                                    {txn.deliveryNo} {txn.proformaNo}
+                                    {txn.debitNoteNo} {txn.creditNoteNo}
+                                    {txn.paymentNumberIn} {txn.paymentNumberOut}
+                                </TableCell>
+                                <TableCell>{txn.partyName}</TableCell>
+                                <TableCell>{txn.totalAmount}</TableCell>
+                                <TableCell align="center">
+                                    <IconButton
+                                        aria-label="edit"
+                                        onClick={() => handleBooleanChangeView(txn.id)}
+                                    >
+                                        <ArticleIcon/>
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))}
                 </TableBody>
             </Table>
             <Link component={RouterLink} to="/transactions" style={{margin: '10px'}}>
